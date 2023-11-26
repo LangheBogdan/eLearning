5c2eda435a5c4d540f6062f55681145a2f6171ff
Bogdan Langhe <bogdanlanghe@gmail.com>
2023-11-26 17:01:59 +0200

    [200~Improve image responsiveness in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js}", "./index.html"],
+  content: ["./src/**/*.js", "./index.html"],
   theme: {
     fontFamily: {
       sans: ["Lato", "ui-sans-serif"],
